424ccda40fee3a1c9e47defbfd781af8f9848800
Santino33 <davidnaranjo330@gmail.com>
2024-02-13 21:09:03 -0500

    Error bucle

---
+++ proyecto/src/main/java/co/edu/uptc/proyecto/services/enlazadas/Nodo.java
@@ -0,0 +1,33 @@
+package co.edu.uptc.proyecto.services.enlazadas;
+import java.util.Objects;
+
+public class Nodo<T> {
+
+    private T info;
+    private Nodo next;
+
+
+    public Nodo() {
+
+    }
+
+
+    public T getInfo() {
+        return this.info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public Nodo getNext() {
+        return this.next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+    
+
+
+}
